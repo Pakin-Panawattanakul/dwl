1a3cb9cff18093ab24990aeff39ee84b4257571e
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-07-14 00:05:34 +0700

    remove monitor config patches

--- config.def.h
+++ config.def.h
@@ -66,14 +66,10 @@ static const Layout layouts[] = {
  * WARNING: negative values other than (-1, -1) cause problems with Xwayland clients due to
  * https://gitlab.freedesktop.org/xorg/xserver/-/issues/899 */
 static const MonitorRule monrules[] = {
-   /*    name  mfact nmaster  scale       layout              rotate/reflect   x   y  resx resy      rate mode adaptive */
-   /*{"eDP-1",  0.5f,      1,     2, &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,  0,  0,    0,   0, 120.000f,   1,       1}, 	/* example of a HiDPI laptop monitor at 120Hz: */
-   /*
-	* mode lets the user decide how dwl should implement the modes:
-	* -1 sets a custom mode following the user's choice
-	* All other numbers set the mode at the index n; 0 is the standard mode; see wlr-randr
-	*/
-	{     NULL, 0.55f,     1,     1, &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL, -1, -1,    0,   0,     0.0f,   0,       1},
+   /* name        mfact  nmaster scale layout       rotate/reflect                x    y
+    * example of a HiDPI laptop monitor:
+    { "eDP-1",    0.5f,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 }, */
+	{ NULL,       0.55f, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	/* default monitor rule: can be changed but cannot be eliminated; at least one monitor rule must exist */
 };
 
